aea09caca446084361a0df3c2c3656e620ff64d2
Hunter Land <hunterland457@gmail.com>
2023-10-11 21:22:30 -0700

    Make use of string conversion functions for permutation test names

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,26 +5,42 @@
 #include "tests.cpp"
 #include <filesystem>
 
+#include <btf/stringLookup.hpp>
+#include "utility.hpp"
+
 int main(int argc, char** argv) {
 
-	btf::typeMatrix.valuesForType<sks::domain>() = {
-		sks::IPv4,
-		sks::IPv6,
-		sks::unix,
-		#ifdef has_ax25
-		sks::ax25,
-		#endif
-	};
-	btf::typeMatrix.valuesForType<sks::type>() = {
-		sks::stream,
-		sks::dgram,
-		sks::seq,
-	};
+	//Set up type matrix and string conversion lookup
+	{
+		btf::typeMatrix.valuesForType<sks::domain>() = {
+			sks::IPv4,
+			sks::IPv6,
+			sks::unix,
+			#ifdef has_ax25
+			sks::ax25,
+			#endif
+		};
+		btf::typeMatrix.valuesForType<sks::type>() = {
+			sks::stream,
+			sks::dgram,
+			sks::seq,
+		};
+		btf::typeMatrix.valuesForType<std::chrono::milliseconds>() = {
+			std::chrono::milliseconds(0),
+			std::chrono::milliseconds(100),
+		};
 
-	btf::addTestPermutations("Sockets can communicate",         {"2", "smoke"}, socketsSendAndReceive);
-	btf::addTestPermutations("Addresses cast correctly",        {"3", "smoke"}, addressesMatchCEquivalents);
-	btf::addTestPermutations("Socket pairs can be created",     {"4"},          socketPairsCanBeCreated);
-	btf::addTestPermutations("readReady() times-out correctly", {"5"},          readReadyTimesOutCorrectly);
+		btf::typeStrings.setFunction<sks::domain>(str);
+		btf::typeStrings.setFunction<sks::type>(str);
+		btf::typeStrings.setFunction(&sks::address::name);
+		btf::typeStrings.setFunction<std::chrono::milliseconds>(str);
+	}
+
+	//Add tests
+	btf::addTestPermutations("Sockets can communicate (%0, %1)",             {"2", "smoke"}, socketsSendAndReceive);
+	btf::addTestPermutations("Addresses cast correctly (%0)",                {"3", "smoke"}, addressesMatchCEquivalents);
+	btf::addTestPermutations("Socket pairs can be created (%0)",             {"4"},          socketPairsCanBeCreated);
+	btf::addTestPermutations("readReady() times-out correctly (%0, %1, %2)", {"5"},          readReadyTimesOutCorrectly);
 
 	//Print info before run starts
 	btf::preRun = [](std::vector<btf::test> testsToRun, size_t threadCount) -> void{
@@ -32,6 +48,7 @@ int main(int argc, char** argv) {
 		std::cout << std::endl;
 	};
 
+	//Clean up any files we may have created
 	btf::postTest = [](std::ostream& log, btf::test t, btf::testResult r) -> void{
 		//Remove any old socket files (unix sockets)
 		for (size_t i = 0; i < 2; i++) {
